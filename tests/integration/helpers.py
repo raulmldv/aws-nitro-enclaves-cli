3864eda95d8223ce7114d6a28150e1c8cdac2732
Raul-Ovidiu Moldovan <raulmldv@amazon.com>
2021-07-29 17:04:46 +0300

    Implement command 'describe-eif'
    New command is added for describing an EIF at a given path.
    Usage example: 'nitro-cli describe-eif --eif-path dir/test.eif'.
    Information about the EIF version, signing certificate, PCRs
    is parsed from the file sections and displayed as JSON. After
    the implementation of metadata in EIF v4, this section will be
    displayed as well.

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -129,6 +129,16 @@ def describe_enclaves_ok():
 
         return run_subprocess_ok(args)
 
+# Runs describe_eif command, describing the EIF at the given path
+# Checks if command is succesful and returns a CompletedProcess
+def describe_eif_ok(eif_name):
+        eif_path = TEST_IMAGES + eif_name
+        args = [ "nitro-cli",
+                 "describe-eif",
+                 "--eif-path", eif_path,
+                ]
+        return run_subprocess_ok(args)
+
 # Connects to the enclave console defined by the enclave id
 # Returns the handle to the running process.
 def connect_console(enclave_id):
